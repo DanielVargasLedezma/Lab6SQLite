9d2cd8b328c24cb6e76743dd9b90d9e8ddc55cd4
valeriatch <71364567+valeriatch@users.noreply.github.com>
2022-06-18 18:42:33 -0600

    Cursos

---
+++ app/src/main/java/com/example/lab6sqlite/ui/database/Curso.kt
@@ -0,0 +1,3 @@
+package com.example.lab6sqlite.ui.database
+
+data class Curso (var id: String = "", var descripcion: String = "", var creditos: Int = 0)
---
+++ app/src/main/java/com/example/lab6sqlite/ui/database/CursoDataBaseHelper.kt
@@ -0,0 +1,78 @@
+package com.example.lab6sqlite.ui.database
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class CursoDataBaseHelper (context: Context):
+    SQLiteOpenHelper(context, DATABASE_NAME, null, 1) {
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(
+            "CREATE TABLE $TABLE_NAME ($COL_1 TEXT PRIMARY KEY, $COL_2 TEXT, $COL_3 INTEGER)"
+        )
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+        onCreate(db)
+    }
+
+    fun insertCurso(id: String, descripcion: String, creditos: Int) {
+        val db = this.writableDatabase
+
+        if (db.isOpen) {
+            val contentValues = ContentValues()
+            contentValues.put(COL_1, id)
+            contentValues.put(COL_2, descripcion)
+            contentValues.put(COL_3, creditos)
+
+            db.insert(TABLE_NAME, null, contentValues)
+            db.close()
+        }
+    }
+    fun deleteCurso(id: String): Int {
+        val db = this.writableDatabase
+
+        if(db.isOpen){
+            var usersDeleted = db.delete (TABLE_NAME,"ID = ?", arrayOf(id))
+            db.close()
+            return usersDeleted
+        }
+        return 0
+    }
+    fun updateCurso (id: String, descripcion: String, creditos: Int): Boolean {
+
+        val db = this.writableDatabase
+        if(db.isOpen){
+            val contentValues = ContentValues()
+            contentValues.put(COL_1, id)
+            contentValues.put(COL_2, descripcion)
+            contentValues.put(COL_3, creditos)
+            db.update(TABLE_NAME, contentValues, "ID = ?", arrayOf(id))
+            db.close()
+            return true
+        }
+        return false
+    }
+    val allCursos : Cursor?
+        get() {
+            val db = this.writableDatabase
+            if(db.isOpen){
+                val res = db.rawQuery("SELECT * FROM " + TABLE_NAME, null)
+                return res
+            }
+            return null
+        }
+
+    companion object {
+        val DATABASE_NAME = "matricula.db"
+        val TABLE_NAME = "cursos"
+        val COL_1 = "ID"
+        val COL_2 = "DESCRIPCION"
+        val COL_3 = "CREDITOS"
+
+    }
+}
